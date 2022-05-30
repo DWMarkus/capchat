171032a0f06fd08aefd14a844a5411a69da200af
DWMarkus <gapasin.marc@yahoo.fr>
2022-05-30 18:10:24 +0200

    display html properly

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 const fastify = require('fastify')({ logger: true })
+const fs = require('fs')
+const path = require('path')
 
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
-})
+fastify.get('/', (_request, reply) => sendHTMLFile(reply, 'index.html'))
 
-const start = async () => {
-  try {
-    await fastify.listen(3000)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
+// Return HTML file
+function sendHTMLFile(reply, file) {
+    reply.type('text/html').send(fs.readFileSync(`public/${file}`, {encoding: 'utf-8'}))
 }
-start()
+
+// Start the server
+const start = async () => {
+    try {
+      await fastify.listen(3000);
+    } catch (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+  };
+  start();
+
+  // Declare a named route
+fastify.get('/test', async (request, reply) => {
+    return { hello: 'user' };
+  });
+
+  // Declare a dynamic route
+fastify.get('/name/:name', async (request, reply) => {
+    return { hello: request.params.name };
+  });
+
+  // Declare a post route
+fastify.post('/dump', async (request, reply) => {
+    return request.body;
+  });
